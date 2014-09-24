ab3674e1b32f58d34f7e393e1e7307daaa87512b
andyqee <vitasone@gmail.com>
2014-09-24 17:15:33 +0800

    Site updated at 2014-09-24 09:15:33 UTC

--- downloads/code/leetcode/grayCode.cpp
+++ downloads/code/leetcode/grayCode.cpp
@@ -1,23 +1,18 @@
-
 class Solution {
 public:
     vector<int> grayCode(int n) {
         std::vector<int> result;
-        if (n == 0)
-        {
-        	result.push_back(0);
-        } else {
-        	for (int i = 0; i < n; ++i)
-        	{
-        		int adder = 1 << i;
-        		int len = result.size();
-        		for (int i = len -1 ; i >= 0; --i)
-        		{
-        			int element = result[i] + adder;
-        			result.push_back(element);
-        		}
-        	}
-        }
+            result.push_back(0);
+            for (int i = 0; i < n; ++i)
+            {
+                int adder = 1 << i;
+                int len = result.size();
+                for (int i = len -1 ; i >= 0; --i)
+                {
+                    int element = result[i] + adder;
+                    result.push_back(element);
+                }
+            }
         return result;
     }
 };
